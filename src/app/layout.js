4aec0b497fceac64289d808ef65a8b9b6307c0f8
Jeremy Su <70295411+bulletchicken@users.noreply.github.com>
2026-01-05 14:48:15 -0500

    snappier smooth scroll

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,12 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.8,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: "vertical",
       gestureOrientation: "vertical",
       smoothWheel: true,
-      wheelMultiplier: 0.5,
+      wheelMultiplier: 0.95,
       smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
